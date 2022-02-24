39251210fd0af12a420c9b52195f0fafa3d6b020
Carolig19 <ciribarrenguerra@gmail.com>
2022-02-24 14:46:16 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let numero = ["A";"2";"3";"4";"5";"6";"7";"8";"9";"10";"11";"12"];
+let pinta = ["♦";"♥";"♠";"♣"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let RandCard = () => {
+let NumRandom = numero (math.floor(math.random())* numero.lenght);
+let PintRandom = pinta (math.floor(math.random()) * pinta.length);
+    }
+
+}};
