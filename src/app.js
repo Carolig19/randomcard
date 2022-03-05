854df67df6f5bf0a1ee39660a7e9b816804bcc65
Carolig19 <ciribarrenguerra@gmail.com>
2022-03-05 13:36:07 +0000

    randomcard

--- src/app.js
+++ src/app.js
@@ -4,14 +4,3 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-let numero = ["A";"2";"3";"4";"5";"6";"7";"8";"9";"10";"11";"12"];
-let pinta = ["♦";"♥";"♠";"♣"];
-
-window.onload = function() {
-    let RandCard = () => {
-let NumRandom = numero (math.floor(math.random())* numero.lenght);
-let PintRandom = pinta (math.floor(math.random()) * pinta.length);
-    }
-
-}};
